1df2ff634099d41ca37c7f32da8b18842cab12fc
Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
2024-07-26 13:57:32 +0200

    ⚗️ demonstrate menu controller problem

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,20 +1,41 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonMenu } from '@ionic/angular/standalone';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonMenu],
 })
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  constructor(private menuCtrl: MenuController) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+
+  clickTestGetMenus() {
+    this.menuCtrl.getMenus().then(result => console.debug('getMenus', result));
+  }
+
+  clickTestOpen() {
+    this.menuCtrl.open().then(result => console.debug('open', result))
+  }
+
+  clickTestOpenWithId() {
+    this.menuCtrl.open('first-menu').then(result => console.debug('open with ID', result));
+  }
+
+  clickTestClose() {
+    this.menuCtrl.close().then(result => console.debug('close', result));
+  }
+
+  clickTestCloseWithId() {
+    this.menuCtrl.close('first-menu').then(result => console.debug('close with ID', result));
   }
 }
